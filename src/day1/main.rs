033a787b15d92212e7f161d5a6acab5927646199
Saquib <saquib777@yahoo.com>
2023-12-09 11:28:03 +0000

    day1

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,23 +1,36 @@
 use std::fs;
 
 fn main() {
-    // Read file
     let file_path = "./input.txt";
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Could not read file");
+    let contents = fs::read_to_string(file_path).expect("Could not read file");
 
-    // Read line by line
-    // let mut result = Vec::new();
-    let mut sum = 0;
+    let mut sum: u16 = 0;
 
     for line in contents.lines() {
+        let mut l = 0;
+        let mut r = line.len() - 1;
+        let zero = '0' as u16;
 
-        println!("{}",line);
+        while l <= r {
+            let left_char = line.chars().nth(l).unwrap();
+            let right_char = line.chars().nth(r).unwrap();
+
+            if left_char.is_numeric() && right_char.is_numeric() {
+                sum += 10 * (left_char as u16 - zero);
+                sum += right_char as u16 - zero;
+                break;
+            }
+
+            if !left_char.is_numeric() {
+                l += 1;
+            }
+
+            if !right_char.is_numeric() {
+                r -= 1;
+            }
+        }
     }
 
-    sum = 1;
-
-    // println!("{:?}", result)
     println!("{}", sum);
 }
